546f9503c45cf3884b80e49c484e1868d05363bf
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2023-04-23 18:31:46 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const box = document.querySelector(".carrossel")
+const imagens = document.querySelectorAll(".carrossel img")
+let contador = 0
+function slider(){
+    contador++
+    if (contador > imagens.length - 1){
+        contador = 0
+    }
+    box.style.transform = `translateX(${-contador * 1250}px)`
+}
+setInterval(slider, 2000)
