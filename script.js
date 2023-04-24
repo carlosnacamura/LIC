e93dcbd82fe7b02977ba36c49b5dd016dea5a996
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2023-04-23 22:25:47 -0300

    LIC pronto

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ function slider(){
     if (contador > imagens.length - 1){
         contador = 0
     }
-    box.style.transform = `translateX(${-contador * 1250}px)`
+    box.style.transform = `translateX(${-contador * 100}vw)`
 }
-setInterval(slider, 2000)
+setInterval(slider, 4000)
+
+/* Zoom */
+const caixa = document.getElementById("box")
+const img = document.querySelector("#box img")
+caixa.addEventListener("mousemove",(e)=>{
+const x = e.clientX - e.target.offsetLeft
+const y = e.clientY - e.target.offsetTop
+img.style.transformOrigin = `${x}px ${y}px`
+img.style.transform = "scale(1.2)"
+img.style.transition = "1s ease"
+})
+
+caixa.addEventListener("mouseleave",()=>{
+img.style.transformOrigin = "center center"
+img.style.transform = "scale(1)"
+img.style.transition = "1s ease"
+})
